7bddb9360ff59becb4d90c07dad2604717ddca5c
jeherrera <jeherrera@ebay.com>
2014-10-12 21:30:38 +0200

    added my code

--- README.md
+++ README.md
@@ -2,25 +2,5 @@
 
 Hello peer. Thank you for taking the time to evaluate my project.
 
-The result of my work can be found in the images below. Also you can check the R code in each of the files above,
-
-### Plot 1
-
-
-![plot of plot 1](plot1.png) 
-
-
-### Plot 2
-
-![plot of plot 2](plot2.png) 
-
-
-### Plot 3
-
-![plot of plot 3](plot3.png) 
-
-
-### Plot 4
-
-![plot of plot 4](plot4.png) 
+The result of my work can be found in the images and R code in each of the files above.
 
--- plot1.R
+++ plot1.R
@@ -1,3 +1,5 @@
+setwd("/Users/jeherrera/Box Sync/Courses/Data Science - Specialization/4 - Exploratory Data Analysis/Week 1/ExData_Plotting1")
+
 temp <- tempfile()
 download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", temp)
 consumption <- read.csv(unz(temp, "household_power_consumption.txt"), sep=";", na.strings="?")
@@ -7,5 +9,5 @@ consumption <- consumption[which(!is.na(consumption)),]
 consumption$Date <- as.Date(consumption$Date, "%d/%m/%Y")
 consumption <- subset(consumption, Date >= "2007-02-01" & Date <= "2007-02-02")
 png("plot1.png")
-hist(consumption$Global_active_power, col="red", xlab="Global Active Power (kilowatts)")
+hist(consumption$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main = "Global Active Power")
 dev.off()
